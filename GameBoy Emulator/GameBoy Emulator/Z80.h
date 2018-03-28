fda55099619a037bffb3d216a508cd480ffd25e8
amp287 <guitar12amp@aol.com>
2018-03-28 00:23:46 -0400

    Start instruction mapping

--- GameBoy Emulator/GameBoy Emulator/Z80.h
+++ GameBoy Emulator/GameBoy Emulator/Z80.h
@@ -179,15 +179,15 @@ static INSTR Opcodes[] = {
 	{"INC BC", NULL, BC, NULL},		//0x03
 	{"INC B", NULL, B, NULL},			//0x04
 	{"DEC B", NULL, B, NULL},			//0x05
-	{"LD B, n", LD_nn_n, B, NULL},		//0x06
+	{"LD B, n", LD_nn_n, B, READ_8},		//0x06
 	{"RLC A", NULL, A, NULL},			//0x07
 	{"LD nn, SP", NULL, NULL, SP},	//0x08
 	{"ADD HL, BC", NULL, HL, BC},	//0x09
-	{"LD A, BC", NULL, A, BC},		//0x0A
+	{"LD A, BC", LD_A_n, A, BC},		//0x0A
 	{"DEC BC", NULL, BC, NULL},		//0x0B
 	{"INC C", NULL, C, NULL},			//0x0C
 	{"INC D", NULL, D, NULL},			//0x0D
-	{"LD C, n", NULL, C, NULL},		//0x0E
+	{"LD C, n", LD_nn_n, C, READ_8},		//0x0E
 	{"RRC A", NULL, A, NULL},			//0x0F
 	{"STOP", NULL, NULL, NULL},			//0x10
 	{"LD DE, nn", NULL, DE, NULL},	//0x11
@@ -195,15 +195,15 @@ static INSTR Opcodes[] = {
 	{"INC DE", NULL, DE, NULL},		//0x13
 	{"INC D", NULL, D, NULL},			//0x14
 	{"DEC D", NULL, D, NULL},			//0x15
-	{"LD D, n", NULL, D, NULL},		//0x16
+	{"LD D, n", LD_nn_n, D, READ_8},		//0x16
 	{"RL A", NULL, A, NULL},			//0x17
 	{"JR n", NULL, NULL, NULL},			//0x18
 	{"ADD HL, DE", NULL, HL, DE},	//0x19
-	{"LD A, DE", NULL, A, DE},		//0x1A
+	{"LD A, DE", LD_A_n, A, DE},		//0x1A
 	{"DEC DE", NULL, DE, NULL},		//0x1B
 	{"INC E", NULL, E, NULL},			//0x1C
 	{"DEC E", NULL, E, NULL},			//0x1D
-	{"LD E, n", NULL, E, NULL},		//0x1E
+	{"LD E, n", LD_nn_n, E, READ_8},		//0x1E
 	{"RR A", NULL, A, NULL},			//0x1F
 	{"JR NZ, n", NULL, NZ, NULL},		//0x20		//NZ is check for Not zero
 	{"LD HL, nn", NULL, HL, NULL},	//0x21
@@ -211,7 +211,7 @@ static INSTR Opcodes[] = {
 	{"INC HL", NULL, HL, NULL},		//0x23
 	{"INC H", NULL, H, NULL},			//0x24
 	{"DEC H", NULL, H, NULL},			//0x25
-	{"LD H, n", NULL, H, NULL},		//0x26
+	{"LD H, n", LD_nn_n, H, READ_8},		//0x26
 	{"DAA", NULL, NULL, NULL},			//0x27
 	{"JR Z, n", NULL, Z, NULL},		//0x28		//Z is check for zero
 	{"ADD HL, HL", NULL, HL, HL},	//0x29
@@ -219,7 +219,7 @@ static INSTR Opcodes[] = {
 	{"DEC HL", NULL, HL, NULL},		//0x2B
 	{"INC L", NULL, L, NULL},			//0x2C
 	{"DEC L", NULL, L, NULL},			//0x2D
-	{"LD L, n", NULL, L, NULL},		//0x2E
+	{"LD L, n", LD_nn_n, L, READ_8},		//0x2E
 	{"CPL", NULL, NULL, NULL},			//0x2F
 	{"JR NC, n", NULL, NC, NULL},		//0x30		//NC is check for no carry
 	{"LD SP, nn", NULL, SP, NULL},	//0x31
@@ -227,7 +227,7 @@ static INSTR Opcodes[] = {
 	{"INC SP", NULL, SP, NULL},		//0x33
 	{"INC HL", NULL, HL, NULL},		//0x34
 	{"DEC HL", NULL, HL, NULL},		//0x35
-	{"LD HL, n", NULL, HL, NULL},		//0x36
+	{"LD HL, n", LD_r1_r2, HL, READ_8},		//0x36 CHECK IF READ 8
 	{"SCF", NULL, NULL, NULL},			//0x37
 	{"JR C, n", NULL, C, NULL},		//0x38		//C is check for carry
 	{"ADD HL, SP", NULL, HL, SP},	//0x39
@@ -235,72 +235,72 @@ static INSTR Opcodes[] = {
 	{"DEC SP", NULL, SP, NULL},		//0x3B
 	{"INC A", NULL, A, NULL},			//0x3C
 	{"DEC A", NULL, A, NULL},			//0x3D
-	{"LD A, n", NULL, A, NULL},		//0x3E
+	{"LD A, n", LD_A_imm, A, READ_8},		//0x3E
 	{"CCF", NULL, NULL, NULL},			//0x3F
-	{"LD B, B", NULL, B, B},		//0x40
-	{"LD B, C", NULL, B, C},		//0x41
-	{"LD B, D", NULL, B, D},		//0x42
-	{"LD B, E", NULL, B, E},		//0x43
-	{"LD B, H", NULL, B, H},		//0x44
-	{"LD B, L", NULL, B, L},		//0x45
-	{"LD B, HL", NULL, B, HL},		//0x46
+	{"LD B, B", LD_r1_r2, B, B},		//0x40
+	{"LD B, C", LD_r1_r2, B, C},		//0x41
+	{"LD B, D", LD_r1_r2, B, D},		//0x42
+	{"LD B, E", LD_r1_r2, B, E},		//0x43
+	{"LD B, H", LD_r1_r2, B, H},		//0x44
+	{"LD B, L", LD_r1_r2, B, L},		//0x45
+	{"LD B, HL", LD_r1_r2, B, HL},		//0x46
 	{"LD B, A", NULL, B, A},		//0x47
-	{"LD C, B", NULL, C, B},		//0x48
-	{"LD C, C", NULL, C, C},		//0x49
-	{"LD C, D", NULL, C, D},		//0x4A
-	{"LD C, E", NULL, C, E},		//0x4B
-	{"LD C, H", NULL, C, H},		//0x4C
-	{"LD C, L", NULL, C, L},		//0x4D
-	{"LD C, HL", NULL, C, HL},		//0x4E
+	{"LD C, B", LD_r1_r2, C, B},		//0x48
+	{"LD C, C", LD_r1_r2, C, C},		//0x49
+	{"LD C, D", LD_r1_r2, C, D},		//0x4A
+	{"LD C, E", LD_r1_r2, C, E},		//0x4B
+	{"LD C, H", LD_r1_r2, C, H},		//0x4C
+	{"LD C, L", LD_r1_r2, C, L},		//0x4D
+	{"LD C, HL", LD_r1_r2, C, HL},		//0x4E
 	{"LD C, A", NULL, C, A},		//0x4F
-	{"LD D, B", NULL, D, B},		//0x50
-	{"LD D, C", NULL, D, C},		//0x51
-	{"LD D, D", NULL, D, D},		//0x52
-	{"LD D, E", NULL, D, E},		//0x53
-	{"LD D, H", NULL, D, H},		//0x54
-	{"LD D, L", NULL, D, L},		//0x55
-	{"LD D, HL", NULL, D, HL},		//0x56
+	{"LD D, B", LD_r1_r2, D, B},		//0x50
+	{"LD D, C", LD_r1_r2, D, C},		//0x51
+	{"LD D, D", LD_r1_r2, D, D},		//0x52
+	{"LD D, E", LD_r1_r2, D, E},		//0x53
+	{"LD D, H", LD_r1_r2, D, H},		//0x54
+	{"LD D, L", LD_r1_r2, D, L},		//0x55
+	{"LD D, HL", LD_r1_r2, D, HL},		//0x56
 	{"LD D, A", NULL, D, A},		//0x57
-	{"LD E, B", NULL, E, B},		//0x58
-	{"LD E, C", NULL, E, C},		//0x59
-	{"LD E, D", NULL, E, D},		//0x5A
-	{"LD E, E", NULL, E, E},		//0x5B
-	{"LD E, H", NULL, E, H},		//0x5C
-	{"LD E, L", NULL, E, L},		//0x5D
-	{"LD E, HL", NULL, E, HL},		//0x5E
+	{"LD E, B", LD_r1_r2, E, B},		//0x58
+	{"LD E, C", LD_r1_r2, E, C},		//0x59
+	{"LD E, D", LD_r1_r2, E, D},		//0x5A
+	{"LD E, E", LD_r1_r2, E, E},		//0x5B
+	{"LD E, H", LD_r1_r2, E, H},		//0x5C
+	{"LD E, L", LD_r1_r2, E, L},		//0x5D
+	{"LD E, HL", LD_r1_r2, E, HL},		//0x5E
 	{"LD E, A", NULL, E, A},		//0x5F
-	{"LD H, B", NULL, H, B},		//0x60
-	{"LD H, C", NULL, H, C},		//0x61
-	{"LD H, D", NULL, H, D},		//0x62
-	{"LD H, E", NULL, H, E},		//0x63
-	{"LD H, H", NULL, H, H},		//0x64
-	{"LD H, L", NULL, H, L},		//0x65
-	{"LD H, HL", NULL, H, HL},		//0x66
+	{"LD H, B", LD_r1_r2, H, B},		//0x60
+	{"LD H, C", LD_r1_r2, H, C},		//0x61
+	{"LD H, D", LD_r1_r2, H, D},		//0x62
+	{"LD H, E", LD_r1_r2, H, E},		//0x63
+	{"LD H, H", LD_r1_r2, H, H},		//0x64
+	{"LD H, L", LD_r1_r2, H, L},		//0x65
+	{"LD H, HL", LD_r1_r2, H, HL},		//0x66
 	{"LD H, A", NULL, H, A},		//0x67
-	{"LD L, B", NULL, L, B},		//0x68
-	{"LD L, C", NULL, L, C},		//0x69
-	{"LD L, D", NULL, L, D},		//0x6A
-	{"LD L, E", NULL, L, E},		//0x6B
-	{"LD L, H", NULL, L, H},		//0x6C
-	{"LD L, L", NULL, L, L},		//0x6D
-	{"LD L, HL", NULL, L, HL},		//0x6E
+	{"LD L, B", LD_r1_r2, L, B},		//0x68
+	{"LD L, C", LD_r1_r2, L, C},		//0x69
+	{"LD L, D", LD_r1_r2, L, D},		//0x6A
+	{"LD L, E", LD_r1_r2, L, E},		//0x6B
+	{"LD L, H", LD_r1_r2, L, H},		//0x6C
+	{"LD L, L", LD_r1_r2, L, L},		//0x6D
+	{"LD L, HL", LD_r1_r2, L, HL},		//0x6E
 	{"LD L, A", NULL, L, A},		//0x6F
-	{"LD HL, B", NULL, HL, B},		//0x70
-	{"LD HL, C", NULL, HL, C},		//0x71
-	{"LD HL, D", NULL, HL, D},		//0x72
-	{"LD HL, E", NULL, HL, E},		//0x73
-	{"LD HL, H", NULL, HL, H},		//0x74
-	{"LD HL, L", NULL, HL, L},		//0x75
+	{"LD HL, B", LD_r1_r2, HL, B},		//0x70
+	{"LD HL, C", LD_r1_r2, HL, C},		//0x71
+	{"LD HL, D", LD_r1_r2, HL, D},		//0x72
+	{"LD HL, E", LD_r1_r2, HL, E},		//0x73
+	{"LD HL, H", LD_r1_r2, HL, H},		//0x74
+	{"LD HL, L", LD_r1_r2, HL, L},		//0x75
 	{"HALT", NULL, NULL, NULL},		//0x76
 	{"LD HL, A", NULL, HL, A},		//0x77
-	{"LD A, B", NULL, A, B},		//0x78
-	{"LD A, C", NULL, A, C},		//0x79
-	{"LD A, D", NULL, A, D},		//0x7A
-	{"LD A, E", NULL, A, E},		//0x7B
-	{"LD A, H", NULL, A, H},		//0x7C
-	{"LD A, L", NULL, A, L},		//0x7D
-	{"LD A, HL", NULL, A, HL},		//0x7E
-	{"LD A, A", NULL, A, A},		//0x7F
+	{"LD A, B", LD_r1_r2, A, B},		//0x78
+	{"LD A, C", LD_r1_r2, A, C},		//0x79
+	{"LD A, D", LD_r1_r2, A, D},		//0x7A
+	{"LD A, E", LD_r1_r2, A, E},		//0x7B
+	{"LD A, H", LD_r1_r2, A, H},		//0x7C
+	{"LD A, L", LD_r1_r2, A, L},		//0x7D
+	{"LD A, HL", LD_r1_r2, A, HL},		//0x7E
+	{"LD A, A", LD_r1_r2, A, A},		//0x7F
 	{"ADD A, B", NULL, A, B},		//0x80
 	{"ADD A, C", NULL, A, C},		//0x81
 	{"ADD A, D", NULL, A, D},		//0x82
@@ -423,7 +423,7 @@ static INSTR Opcodes[] = {
 	{"RST 30", NULL, "30", NULL},		//0xF7
 	{"LDHL SP, d", NULL, SP, NULL},	//0xF8
 	{"LD SP, HL", NULL, SP, HL},	//0xF9
-	{"LD A, nn", NULL, A, NULL},		//0xFA
+	{"LD A, nn", LD_A_nn, A, READ_16},		//0xFA
 	{"EI", NULL, NULL, NULL},			//0xFB
 	{"XX", NULL, NULL, NULL},			//0xFC
 	{"XX", NULL, NULL, NULL},			//0xFD
